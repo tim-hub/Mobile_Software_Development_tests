093695647a6cc1413741df6cfd4ba01eaf47c0c5
tim-hub <tim_w@outlook.com>
2018-03-08 12:57:29 +1300

    keep drawing on touch

---
+++ README.md
@@ -0,0 +1,4 @@
+## About
+These are some testing projects I developed in Mobile Software Development.
+
+All rights reserved, only test and sample, no exams.
--- Week2Draw2/app/src/main/java/uno/bai/tim/week2draw2/MyImage.java
+++ Week2Draw2/app/src/main/java/uno/bai/tim/week2draw2/MyImage.java
@@ -8,17 +8,21 @@ import android.graphics.PointF;
 import android.view.MotionEvent;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+
 /**
  * Created by tim on 8/03/18.
  */
 
-public class MyImage extends ImageView {
+import  android.support.v7.widget.AppCompatImageView;
+
+public class MyImage extends AppCompatImageView {
 
 
-    float x;
-    float y;
+
     PointF pos = new PointF();
     Paint paint = new Paint();
+    ArrayList<PointF> points = new ArrayList<>();
 
 
     public MyImage(Context context){
@@ -29,20 +33,33 @@ public class MyImage extends ImageView {
     public boolean onTouchEvent(MotionEvent motionEvent){
 
 
-        x =motionEvent.getX();
-        y =motionEvent.getY();
+        float x =motionEvent.getX();
+        float y =motionEvent.getY();
         pos = new PointF(x,y);
+        points.add(pos);
+        paint.setColor(Color.GREEN);
+
+        // force a view to draw
         invalidate();
         return true;
-//        return super.onTouchEvent(motionEvent);
+
     }
 
     @Override
     public void onDraw(Canvas canvas){
         super.onDraw(canvas);
 
-        paint.setColor(Color.GREEN);
+        if (points.size()>0){
+            for (int i =0; i< points.size(); i++){
+                pos = points.get(i);
+                canvas.drawCircle(pos.x, pos.y, 50, paint);
 
-        canvas.drawCircle(pos.x,pos.y, 50, paint);
+            }
+
+
+        }
+
+
+//        canvas.drawCircle(pos.x,pos.y, 50, paint);
     }
 }
